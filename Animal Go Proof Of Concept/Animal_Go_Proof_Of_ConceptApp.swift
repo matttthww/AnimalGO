ec206b449838e8f17d328681d02b196a23c96e30
Matthew Castaneda <matthewtree1212@icloud.com>
2025-11-07 19:10:17 -0500

    Initial Commit

---
+++ Animal Go Proof Of Concept/Animal_Go_Proof_Of_ConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_Go_Proof_Of_ConceptApp.swift
+//  Animal Go Proof Of Concept
+//
+//  Created by mattthew on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Animal_Go_Proof_Of_ConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
